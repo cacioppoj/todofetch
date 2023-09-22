752a0000483c31dcceed9df7332d034389d238f4
Jose Daniel Cacioppo Armas <danielcacioppo@gmail.com>
2023-09-22 02:08:55 +0000

    plantilla

---
+++ src/js/component/Title.jsx
@@ -0,0 +1,7 @@
+const Title = () => {
+    return (
+        <h1>Todo App</h1>
+    )
+}
+
+export {Title}
---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,12 @@
+import { Title } from "./Title";
+
+
+const TodoList = () => {
+    return (
+        <div className = "container1">
+            <Title/>
+        </div>
+    )
+}
+
+export {TodoList}
